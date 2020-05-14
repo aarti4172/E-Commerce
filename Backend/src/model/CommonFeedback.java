15138c3a1506d503c36ebd76b2e8bb795af565f2
aarti4172 <61293061+aarti4172@users.noreply.github.com>
2020-05-15 01:02:28 +0530

    Add files via upload

---
+++ Backend/src/model/CommonFeedback.java
@@ -0,0 +1,46 @@
+package com.example.main.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "common_feedback")
+public class CommonFeedback {
+
+	@Id
+	@Column(name = "feedback_id")
+	private int feedbackId;  //(Primary Key)
+	@Column(name = "feedback_subject")
+    private String feedbackSubject;
+	@Column(name = "feedback_message")
+    private String feedbackMessage;
+	@Column(name = "user_id")
+	private int userId;
+	
+    private int feedbackForUserId;  //(Foreign Key)
+	public int getFeedbackId() {
+		return feedbackId;
+	}
+	public void setFeedbackId(int feedbackId) {
+		this.feedbackId = feedbackId;
+	}
+	public String getFeedbackSubject() {
+		return feedbackSubject;
+	}
+	public void setFeedbackSubject(String feedbackSubject) {
+		this.feedbackSubject = feedbackSubject;
+	}
+	public String getFeedbackMessage() {
+		return feedbackMessage;
+	}
+	public void setFeedbackMessage(String feedbackMessage) {
+		this.feedbackMessage = feedbackMessage;
+	}
+	public int getFeedbackForUserId() {
+		return feedbackForUserId;
+	}
+	public void setFeedbackForUserId(int feedbackForUserId) {
+		this.feedbackForUserId = feedbackForUserId;
+	}
+	public CommonFeedback() {
+	}
+}
