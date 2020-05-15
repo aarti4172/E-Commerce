a67d64130027b57471c34dfed1ceb5736e8b9392
khushi-ambastha <49233695+khushi-ambastha@users.noreply.github.com>
2020-05-15 15:45:17 +0530

    Delete MerchantDAO.java

--- Backend/src/dao/MerchantDAO.java
+++
@@ -1,9 +0,0 @@
-package com.store.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.store.entity.Merchant;
-
-public interface MerchantDAO extends JpaRepository<Merchant, Integer> {
-
-}
