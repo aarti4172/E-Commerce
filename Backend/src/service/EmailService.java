615e40d139ae0a213099e61e2dddb3e9041edc11
aarti4172 <61293061+aarti4172@users.noreply.github.com>
2020-05-16 14:01:22 +0530

    Add files via upload

---
+++ Backend/src/service/EmailService.java
@@ -0,0 +1,55 @@
+package com.example.main.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.example.main.model.User;
+import com.example.main.model.ValidationToken;
+import com.example.main.repository.ValidationTokenRepository;
+
+@Service("EmailService")
+public class EmailService {
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	@Autowired
+	private ValidationTokenRepository validationTokenRepository;
+	private Logger logger= LoggerFactory.getLogger(EmailService.class);
+	
+	public void sendVerificationMail(User user) throws MailException{
+		
+		ValidationToken validationToken= new ValidationToken(user);
+		logger.info("token=" + validationToken.toString());
+		validationTokenRepository.save(validationToken);
+		
+		SimpleMailMessage mail = new SimpleMailMessage();
+		mail.setTo(user.geteMail());
+		mail.setSubject("Complete Registration!");
+		mail.setText("To confirm your account, please click here : "
+	            +"http://localhost:9090/confirm-account?token="
+				+ validationToken.getTokenValue());
+		
+		javaMailSender.send(mail);
+	}
+	
+	public void sendWelcomeMail(User user) throws MailException{
+		
+		SimpleMailMessage mail = new SimpleMailMessage();
+		mail.setTo(user.geteMail());
+		mail.setSubject("Welcome to Capstore.");
+		mail.setText("Hi "+ user.getName()+ ",  you have been successfully registered."
+				+ "Thank you for choosing us.");
+		
+		javaMailSender.send(mail);
+		
+	}
+	
+	
+	
+
+}
