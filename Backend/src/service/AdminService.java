d4d7348cc857e021db92b16ed677c66ae663e6cb
khushi-ambastha <49233695+khushi-ambastha@users.noreply.github.com>
2020-05-16 14:40:03 +0530

    Update AdminService.java

--- Backend/src/service/AdminService.java
+++ Backend/src/service/AdminService.java
@@ -9,9 +9,46 @@ public interface AdminService {
 
 	
 	public void addMerchant(MerchantDetails m);
-	public void removeMerchant(MerchantDetails m);
+	public void removeMerchant(Integer id);
 	public List<MerchantDetails> getAllMerchant();
 	public void updateMerchant(MerchantDetails m);
 	public MerchantDetails findMerchantById(Integer id);
+	
+	
+	List<Product> getAllProducts();
+	//List<Product> getProductsbyInventoryId(int invertoryId);
+	Product getProductByProductId(int productId);
+	List<Product> getProductsByCategory(String productCategory);
+	List<Product> getProductsByBrand(String productBrand);
+	List<Product> getProductsByType(String productInfo);
+	List<Product> getFeaturedProducts();
+	
+	
+	//add, remove, view customers using customerrepository
+	void removeCustomerById(int userId);
+	CustomerDetails addCustomer(CustomerDetails customer);
+	CustomerDetails findCustomerById(int userId);
+	CustomerDetails findCustomerByName(String name);
+	List<User> getAllCustomers();//NotWorking
+	
+	
+	//Common feedbacks
+	CommonFeedback addCommonFeedback(CommonFeedback cfd);
+	void removeCommonFeedbackById(int feedbackId);
+	void removeCommonFeedbackByUserId(int userId);
+	List<CommonFeedback> getAllCommonFeedbackByUserId(int userId);//LogicNotWritten
+	CommonFeedback getCommonFeedbackById(int feedbackId);
+	
+	
+	//Product feedbacks
+	ProductFeedback addCommonFeedback(ProductFeedback cfd);
+	void removeProductFeedbackkById(int feedbackId);
+	void removeProductFeedbackByUserId(int userId);
+	List<ProductFeedback> getAllProductFeedbackByUserId(int userId);//LogicNotWritten
+	ProductFeedback getProductFeedbackById(int feedbackId);
+	List<ProductFeedback> getProductFeedbackByProductId(int productId);
+	
+	
+	
 }
 
