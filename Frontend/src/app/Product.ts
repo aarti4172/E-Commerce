55c14eb297c7d572206348524c605c9a743379ab
Avani-128 <54412445+Avani-128@users.noreply.github.com>
2020-05-20 13:47:22 +0530

    Create Product.ts

---
+++ Frontend/src/app/Product.ts
@@ -0,0 +1,16 @@
+export class Product{
+    productId:number;
+	productName:String;
+	productDiscount: number;
+	productImage:String;
+	productPrice: DoubleRange;
+	productRating:number;
+	noOfProductViewed:number;
+	productBrand:String;
+	noOfProducts:number;
+	productInfo:String;
+    productCategory:String;
+    productActivated: Boolean;
+    status:boolean;
+	featured:boolean;
+}
