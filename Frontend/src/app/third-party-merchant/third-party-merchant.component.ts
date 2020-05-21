51610cca7b041c2d5853eb4308749b6034cdf303
khushi-ambastha <49233695+khushi-ambastha@users.noreply.github.com>
2020-05-21 14:37:26 +0530

    Update third-party-merchant.component.ts

--- Frontend/src/app/third-party-merchant/third-party-merchant.component.ts
+++ Frontend/src/app/third-party-merchant/third-party-merchant.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Merchant } from '../Merchant';
 import { AdminServiceService } from '../admin-service.service';
 import { Router } from '@angular/router';
+import { MerchantDetails } from '../models/MerchantDetails';
 
 @Component({
   selector: 'app-third-party-merchant',
@@ -10,26 +11,37 @@ import { Router } from '@angular/router';
 })
 export class ThirdPartyMerchantComponent implements OnInit {
 
-  merchant:Merchant= new Merchant();
-  Merchant_id:number;
-  Name:string;
-  //Username:string;
-  PhoneNo : String;
-  Alternate_phone_no : String;
-  Alternate_email: String;
+  name:string;
+  username:string;
+  password;
+  phone_number : String;
+  alternate_phone_number : String;
+  eMail:String;
+  alternate_email: String;
   check=false;
   status: string;
-  Rating =0;
+  rating =0;
   isApproved;
+  role="Admin";
+  isACtive=false;
+  securityQueston=null;
+  securityAnswer=null;
+  products=null;
+  user_address=null;
+  product_feedback=null;
+  coupons=null;
+  merchant1: MerchantDetails= new MerchantDetails(name,this.username,this.password,this.eMail,this.role,this.isACtive,this.securityQueston,this.securityAnswer,this.phone_number,
+    this.alternate_phone_number,this.alternate_email,this.products,this.user_address,this.product_feedback,this.coupons,this.isApproved,this.rating);
+  
   constructor( private adminService: AdminServiceService, private router: Router) { }
 
   ngOnInit(): void {
   }
-  addMerchant()
+  async addMerchant()
   {
-    console.log(this.merchant);
-   this.adminService.addNewMerchant(this.merchant);
-   
+    console.log(this.merchant1);
+   let response= await this.adminService.addNewMerchant(this.merchant1);
+   console.log(response);
   
    }
 
