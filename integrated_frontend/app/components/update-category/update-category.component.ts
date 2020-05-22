ac0fc99ccf5956e6cc29dbd6e22c44417689614b
Avani-128 <54412445+Avani-128@users.noreply.github.com>
2020-05-22 22:57:59 +0530

    Update update-category.component.ts

--- integrated_frontend/app/components/update-category/update-category.component.ts
+++ integrated_frontend/app/components/update-category/update-category.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AdminService } from 'src/app/service/admin.service';
 
 @Component({
   selector: 'app-update-category',
@@ -8,11 +10,46 @@ import { Router } from '@angular/router';
 })
 export class UpdateCategoryComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  prCategory: String;
+  updateCategory: String;
+  message:any;
+  msg:any;
+  categoryForm: FormGroup;
+  submitted: boolean=false;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private adminService: AdminService, private router: Router) { }
+
+  ngOnInit(){
+    this.categoryForm=this.formBuilder.group({
+      prCategory:['',Validators.required],
+      updateCategory:['',Validators.required]
+    });
   }
- back(){
-    this.router.navigate(['/productList']);
+
+  updateCate(){
+    this.submitted=true;
+    this.prCategory = this.categoryForm.controls.prCategory.value;
+    this.updateCategory = this.categoryForm.controls.updateCategory.value;
+    this.adminService.updateCategoryByCategory(this.prCategory,this.updateCategory).subscribe( data=>{
+      this.prCategory = this.categoryForm.controls.prCategory.value;
+      this.message = data;
+      if(this.message==true){
+        this.msg="You have successfully updated the category";
+        alert(this.msg);
+        this.router.navigate(['/showProduct']);
+    }
+    else{
+      this.msg="Please enter correct category";
+      alert(this.msg);
+      this.router.navigate(['/updateCategory']);
+    }
+  }, err => 
+    { console.log(err.stack);
+    });
   }
+
+  back(){
+    this.router.navigate(['/showProduct']);
+  }
+
 }
