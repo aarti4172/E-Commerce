79379e3cb1a84bc0b56bd72d812f4df75dcbf7e9
khushi-ambastha <49233695+khushi-ambastha@users.noreply.github.com>
2020-05-22 21:30:43 +0530

    Update update-category.component.ts

--- integrated_frontend/app/components/update-category/update-category.component.ts
+++ integrated_frontend/app/components/update-category/update-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-category',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateCategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+ back(){
+    this.router.navigate(['/productList']);
+  }
 }
