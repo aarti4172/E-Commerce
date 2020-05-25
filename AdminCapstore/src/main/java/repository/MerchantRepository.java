cb900c697446822fd4bff7b3b0aa18faf19f366a
Avani-128 <54412445+Avani-128@users.noreply.github.com>
2020-05-25 15:26:27 +0530

    Add files via upload

---
+++ AdminCapstore/src/main/java/repository/MerchantRepository.java
@@ -0,0 +1,14 @@
+package com.example.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.main.model.MerchantDetails;
+
+@Repository("MerchantRepository")
+public interface MerchantRepository extends JpaRepository<MerchantDetails, Integer> {
+
+	MerchantDetails findByName(String name);
+
+	MerchantDetails findByeMail(String eMail);
+}
