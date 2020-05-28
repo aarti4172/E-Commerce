3ae2d0714293066b07d879a9e148b9e65ce5baf8
Avani-128 <54412445+Avani-128@users.noreply.github.com>
2020-05-28 14:00:58 +0530

    Update ProductRepository.java

--- integrated_backend/src/main/java/com/example/main/repository/ProductRepository.java
+++ integrated_backend/src/main/java/com/example/main/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.example.main.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.main.model.Product;
@@ -10,6 +12,9 @@ import com.example.main.model.Product;
 @Repository(value="ProductRepository")
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 
+	List<Product> findAllByProductCategory(String productCategory);
+
+	boolean existsByProductCategory(String productCategory);
 
 	Product findByProductId(int productId);
 
@@ -18,9 +23,8 @@ public interface ProductRepository extends JpaRepository<Product,Integer> {
 	List<Product> findByProductBrand(String productBrand);
 
 	List<Product> findByProductInfo(String productInfo);
-
-	List<Product> findAllByProductCategory(String productCategory);
-
 	
-
+	@Query(value = "SELECT p.user_id FROM products p WHERE p.product_id = :pid", nativeQuery=true)
+	public int findMerchantId(@Param("pid") int productId);
+	
 }
